3e179c52b109f74394c93ea007f7c24f60c727cd
haSong <70552536+poan1221@users.noreply.github.com>
2024-03-27 22:55:04 +0900

    참외밭 _ 코드개선

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -1,30 +1,40 @@
 import sys
 
-K = int(sys.stdin.readline())
+def get_lengths():
+    #E=1, W=2, S=3, N=4
+    total_list = []
+    height_list = []
+    width_list = []
+    #동서쪽으로 움직이는 경우와 남북쪽으로 움직이는 경우를 나눠서 리스트에 넣어준다.
+    for _ in range(6):
+        dir, length = map(int, sys.stdin.readline().split())
+        total_list.append(length)
+        if dir == 1 or dir == 2:
+            width_list.append(length)
+        else:
+            height_list.append(length)
+    return total_list, height_list, width_list
 
-#E=1, W=2, S=3, N=4
-height = []
-width = []
-total = []
-#동서쪽으로 움직이는 경우와 남북쪽으로 움직이는 경우를 나눠서 리스트에 넣어준다.
-for i in range(6):
-    dir, length = map(int, sys.stdin.readline().split())
-    total.append(length)
-    if dir == 1 or dir ==2:
-        width.append(length)
-    else:
-        height.append(length)
+#빈 사각형의 한 쪽 길이 구하기
+def get_small_length(total, n):
+    #최대값의 위치 구하기
+    idx = total.index(max(n))
+    #위의 인덱스의 양쪽의 값의 차의 절대값
+    small_length = abs(total[idx-1] - total[(idx-5 if idx == 5 else idx +1)])
+    return small_length
 
-bigbox = max(height) * max(width)
+def solution():
+    K = int(sys.stdin.readline())
 
-#세로 최대값 
-maxhidx = total.index(max(height))
-#가로최대값 
-maxwidx = total.index(max(width))
+    total, height, width = get_lengths()
 
-#전체 이동에서 세로 최대값의 다음값에서 세로 최대값 이전의 가로값을 빼준것이 작은 사각형의 가로값 
-small1 = abs(total[maxhidx-1] - total[(maxhidx-5 if maxhidx == 5 else maxhidx +1)])
+    #제일 큰 사각형
+    bigbox = max(height) * max(width)
 
-small2 = abs(total[maxwidx-1] - total[(maxwidx-5 if maxwidx == 5 else maxwidx +1)])
-area = bigbox - (small1 * small2)
-print(area*K)
+    #작은 사각형의 길이
+    small_w, small_h = get_small_length(total, width), get_small_length(total, height)
+
+    area = bigbox - (small_w * small_h)
+    return area * K
+
+print(solution())
