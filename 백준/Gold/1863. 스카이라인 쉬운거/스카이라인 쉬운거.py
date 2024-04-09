b1ffcec6ef96be30fb7604d88a52749690d8afbf
haSong <70552536+poan1221@users.noreply.github.com>
2024-04-09 12:36:46 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
@@ -1,32 +1,29 @@
-# 고도가 바뀌는 지점 x,y
-# 건물이 몇 개나 될까.. - 건물은.. y 값이 높아질 때 +1
-# stack 으로 비교해가며 넣어야 함 - 정렬 필요
-
 import sys
-
-# from collections import deque
-
 input = sys.stdin.readline
 n = int(input())
-skyline = list(int(input().split()[1]) for i in range(n))
-skyline.append(0)  # 마지막에 0이 아니면 건물이 세어지지 않아서 추가
 
-# 처음부터 높이비교를 하기 위해 임시 값 넣기
-stack = [0]
+stack = []
 cnt = 0
-for h in skyline:
-    # 높이 비교를 위해 처음에 담아줌
-    height = h
-    # 마지막 높이가 지금 높이보다 크면 (즉 라인이 떨어짐)
-    while stack[-1] > h:
-        # 높이가 기존에 나왔던 높이랑 같으면 안됨
-        if stack[-1] != height:
-            # 라인이 떨어지는데, 기존 높이랑 같지 않으면 건물
-            cnt += 1
-            # 비교 높이 갱신
-            height = stack[-1]
-        # print(f"비교높이 {height}, 지금 높이 {h}")
+
+for _ in range(n):
+    h = int(input().split()[1])
+
+    # 스택이 있는데, 이전 높이가 지금 높이보다 크면, 스택에서 뺴주고 빌딩 추가
+    while stack and stack[-1] > h:
         stack.pop()
-    stack.append(h)
+        cnt += 1
+
+    # 스택이 없거나, 있는데 이전과 같은 높이라면 그냥 스택을 쌓기만 함
+    if not stack or (stack and stack[-1] != h):
+        stack.append(h)
+
+# 아직 남아있는 스택에 대한 건물 추가
+while stack:
+    # 높이가 있어야 하므로
+    if stack[-1] > 0:
+        cnt += 1
+    stack.pop()
+
+# print(stack)
 
 print(cnt)
