cfdef324c9deea831a5907d6a2a9bb04c58f8581
haSong <70552536+poan1221@users.noreply.github.com>
2024-04-10 17:36:16 +0900

    [Gold IV] Title: 오큰수, Time: 920 ms, Memory: 155536 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,13 +1,18 @@
-N = int(input())
+import sys
+input = sys.stdin.readline
 
+def find_NGE():
+    result = [-1] * n
+    stack = []
+
+    for i in range(n):
+        while stack and list[stack[-1]] < list[i]:
+            result[stack.pop()] = list[i]
+        stack.append(i)
+    return result
+
+
+n = int(input())
 list = list(map(int, input().split()))
-result = [-1] * N
 
-stack = []
-
-for i in range(N):
-    while stack and list[stack[-1]] < list[i]:
-        result[stack.pop()] = list[i]
-    stack.append(i)
-
-print(*result)
+print(*find_NGE())
