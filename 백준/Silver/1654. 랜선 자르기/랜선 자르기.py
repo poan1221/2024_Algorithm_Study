4352c648b3fea498eae6d042b445478d8e4d8792
haSong <70552536+poan1221@users.noreply.github.com>
2024-04-12 17:18:12 +0900

    [Silver II] Title: 랜선 자르기, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -4,18 +4,21 @@ input = sys.stdin.readline
 n, k = map(int, input().split())
 cables = [int(input()) for _ in range(n)]
 
+# n 개보다 많이 많들 수 있는지?
+def make_n_cables():
+    count = 0
+    for len in cables:
+        count += len // mid
+        if count >= k:
+            return True
+    return False
+
 left = 1
 right = max(cables)
 
 while left <= right:
     mid = (left + right) // 2
-    
-    count = 0
-    for len in cables:
-        count += len // mid
-    
-    # n 개보다 많이 많들 수 있는지?
-    if count >= k:
+    if make_n_cables():
         left = mid + 1
     else:
         right = mid - 1
