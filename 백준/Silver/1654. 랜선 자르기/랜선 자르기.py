d59fb64a85ae18c7d8c46ef3b02a6085947b5e68
haSong <70552536+poan1221@users.noreply.github.com>
2024-04-12 17:16:29 +0900

    [Silver II] Title: 랜선 자르기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,23 +1,23 @@
-# 이미 가지고 있는 K개의 랜선을 N개의 랜선으로 자르는 것
-# N개를 만들 수 있는 최대 길이를 구하기
+import sys
+input = sys.stdin.readline
 
 n, k = map(int, input().split())
-lines = [int(input()) for _ in range(n)]
-ans = 1 # 정답은 항상 1 이상
+cables = [int(input()) for _ in range(n)]
 
-# 탐색 범위 설정
-# N은 1이상 1,000,000이하의 정수, 랜선의 길이는 2^31-1보다 작거나 같음 -> 1부터 2^31-1 까지..
 left = 1
-right = 2 ** 31 - 1 # 전선 최대 길이
+right = max(cables)
+
 while left <= right:
     mid = (left + right) // 2
-    cnt = 0
-    for line in lines:
-        cnt += line // mid # 전선 길이로 나눈 몫이 만들 수 있는 전선의 개수
-        # 전체 cnt : 전체 자른 전선의 개수
-    if cnt >= k: # k개 이상의 전선을 만들 수 있으면 정답을 갱신!
-        ans = mid
-        left = mid + 1 #탐색범위 재설정
+    
+    count = 0
+    for len in cables:
+        count += len // mid
+    
+    # n 개보다 많이 많들 수 있는지?
+    if count >= k:
+        left = mid + 1
     else:
         right = mid - 1
-print(ans)
+
+print(right)
