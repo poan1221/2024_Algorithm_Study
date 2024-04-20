f82b729adf0cb2782ef9b64cfac9bcc0975317fc
haSong <70552536+poan1221@users.noreply.github.com>
2024-04-20 09:51:44 +0900

    [Gold V] Title: 토마토, Time: 2208 ms, Memory: 98600 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,45 +1,55 @@
-# 익은 토마도 인접하면 하루 뒤에 익게 됨 - 혼자 익는 건 없음 > 일단 익어있는 토마토부터 구해야 함
-# 인접 - 왼쪽 오른쪽 앞 뒤 4방향
-# 다 익는 지 얼마나 걸리는 지 최소 날짜 구하기 - BFS
+# 인접 토마토는 익는다
+# 토마토 위치를 찾고 방향에 맞춰... 탐색 bfs
+import sys
+input = sys.stdin.readline
 
 from collections import deque
 
-# 입력값
+def find_first(dq):
+    for x in range(n):
+        for y in range(m):
+            if metrix[x][y] == 1:
+                dq.append((x, y))
+
+def find_way(x, y, d):
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    return [x + dx[d], y + dy[d]]
+
+def bfs():
+    queue = deque()
+
+    # 토마토 위치 찾기
+    find_first(queue)
+
+    # bfs 시작
+    while queue:
+        x, y = queue.popleft()
+
+        for d in range(4):
+            nx, ny = find_way(x, y, d)
+
+            if 0 <= nx < n and 0 <= ny < m and metrix[nx][ny] == 0:
+                metrix[nx][ny] = metrix[x][y] + 1
+                queue.append((nx, ny))
+
+
+# input
 m, n = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(n)]
+metrix = [list(map(int, input().split())) for _ in range(n)]
 
-# BFS 탐색 초기 위치 찾기
-tomatoes = deque()
-for x in range(n):
-    for y in range(m):
-        if board[x][y] == 1:
-            tomatoes.append((x, y))
+# bfs 로 메트릭스 갱신
+bfs()
 
-# BFS 탐색 시작!
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-while tomatoes:
-    x, y = tomatoes.popleft()
-    for d in range(4):
-        # 한 칸씩 인접한 곳을 방향으로 따져서 다음 방향에 대한 것을 확인
-        nx, ny = x + dx[d], y + dy[d]
-        if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 0:
-            # 처음 시작된 곳부터 +1 씩 늘어남 = 익은 날짜를 세는 것과 같음
-            board[nx][ny] = board[x][y] + 1
-            # 기준 토마토 위치 갱신
-            tomatoes.append((nx, ny))
+# 토마토 모두 익는 최초 날짜 출력 - 안 익은 거 있으면 -1
+final_day = 0
 
-# 답 출력을 위한 함수
-def find_max_day(board):
-    max_day = 0
-    for row in board:
-        for tomato in row:
-            # 못 익은 토마토가 있다면?
-            if tomato == 0:
-                return -1
-            max_day = max(max_day, tomato)
-    # 0 부터 센 거라 -1 해주기
-    return max_day - 1
+for row in metrix:
+    for tomato in row:
+        if tomato == 0 :
+            print(-1)
+            exit()
+        final_day = max(final_day, tomato)
 
-
-print(find_max_day(board))
+print(final_day - 1)
