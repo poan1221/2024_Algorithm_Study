3bf3cc9dc32c8f2f73c0f0b7de13ed94c5c4080b
haSong <70552536+poan1221@users.noreply.github.com>
2024-09-10 21:05:59 +0900

    [level 0] Title: a와 b 출력하기, Time: 69.53 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -1,13 +1,17 @@
-const readline = require('readline');
+const readline = require('readline')
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
+}).on('line', function (line) {
+    const [a, b] = line.split(' ');
+    console.log('a =', a);
+    console.log('b =', b);
 });
 
-let input = [];
+// let input = [];
 
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    console.log(`a = ${Number(input[0])}\nb = ${Number(input[1])}`);
-});
+// rl.on('line', function (line) {
+//     input = line.split(' ');
+// }).on('close', function () {
+//     console.log(`a = ${Number(input[0])}\nb = ${Number(input[1])}`);
+// });
