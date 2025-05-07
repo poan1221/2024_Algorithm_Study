c23bfbd3e3d3e31f744449c528e9d97f807d0061
haSong <70552536+poan1221@users.noreply.github.com>
2025-05-07 11:51:11 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,12 @@
+function solution(ineq, eq, n, m) {
+    const op = ineq + eq;
+    
+    switch(op){
+        case ">=" : return n >= m ? 1 : 0;
+        case "<=" : return n <= m ? 1 : 0;
+        case ">!" : return n > m ? 1 : 0;
+        case "<!" : return n < m ? 1 : 0;
+    }
+    
+
+}
