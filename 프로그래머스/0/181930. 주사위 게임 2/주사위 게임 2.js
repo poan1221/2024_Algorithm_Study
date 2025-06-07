fa8c46813f0ff062084094b71d2276b63cde4b8c
haSong <70552536+poan1221@users.noreply.github.com>
2025-06-07 20:13:33 +0900

    resolve: 주사위게임2 - set 이용 풀이

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -2,12 +2,12 @@ function solution(a, b, c) {
     const sum = a + b + c
     const square = a**2 + b**2 + c**2;
     const cube = a**3 + b**3 + c**3;
+
+    const dsttCnt = new Set([a, b, c]).size;
     
-    if ( a ===b && b === c) {
-        return sum * square * cube;
-    } else if (a ===b || b === c || a === c){
-        return sum * square;
-    } else {
-        return sum;
+    switch(dsttCnt) {
+        case 1 : return sum * square * cube;
+        case 2 : return sum * square;
+        case 3 : return sum;
     }
 }
