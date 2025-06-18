bac34bed5651d61c3e43d633163b80904980c273
haSong <70552536+poan1221@users.noreply.github.com>
2025-06-18 23:17:14 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,14 +1,6 @@
-function solution(n) {
-    const result = [n];
-    
-    while (n !== 1) {
-        if ( n % 2 === 0 ) {
-            n = n/2;
-        } else {
-            n = 3* n + 1
-        }
-        result.push(n);
-    }
-    
-    return result;
+function solution(n, arr = []) {
+    arr.push(n);
+    if (n === 1) return arr;
+    if (n % 2 === 0) return solution(n/2, arr);
+    return solution(3*n+1, arr);
 }
