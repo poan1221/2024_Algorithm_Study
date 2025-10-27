1fb085318971f3617396d4de00a0f7c4b1d03613
haSong <70552536+poan1221@users.noreply.github.com>
2025-10-27 23:14:15 +0900

    [level 1] Title: 약수의 합, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,10 @@
+function solution(n) {
+  let sum = 0;
+  for (let i = 1; i * i <= n; i++) {
+    if (n % i === 0) {
+      sum += i;
+      if (i !== n / i) sum += n / i;
+    }
+  }
+  return sum;
+}
